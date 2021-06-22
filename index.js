b279d7f622a82c5f225489c9328a712eb6057dce
dhersey97 <61208446+dhersey97@users.noreply.github.com>
2021-06-22 14:24:22 -0400

    Added JavaScript for the site
    Added page swapping functionality

---
+++ index.js
@@ -0,0 +1,45 @@
+// Nav Link buttons
+const homeButton = document.getElementById("home-button");
+const aboutButton = document.getElementById("about-button");
+const workButton = document.getElementById("work-button");
+const contactButton = document.getElementById("contact-button");
+
+// Hides the section as well as changes the color of the
+// list item backgrounds back to "inactive" or black
+function hideAll() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("homeItem").style.backgroundColor = 'black';
+
+    document.getElementById("about").style.display = "none";
+    document.getElementById("aboutItem").style.backgroundColor = 'black';
+
+    document.getElementById("work").style.display = "none";
+    document.getElementById("workItem").style.backgroundColor = 'black';
+
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("contactItem").style.backgroundColor = 'black';
+}
+
+// Changes the section as well as the color of the active list item
+function changeView(sectionName, buttonName) {
+    hideAll();
+    document.getElementById(buttonName).style.backgroundColor = '#404040';
+    document.getElementById(sectionName).style.display = "initial";
+}
+
+// Event listeners for the navigation
+homeButton.addEventListener("click", () => {
+    changeView('home', 'homeItem');
+});
+
+aboutButton.addEventListener("click", () => {
+    changeView('about', 'aboutItem');
+});
+
+workButton.addEventListener("click", () => {
+    changeView('work', 'workItem');
+});
+
+contactButton.addEventListener("click", () => {
+    changeView('contact', 'contactItem');
+});
